da3aabab91b002a55ab1c7c9be320a1aa6258cbc
Mohamed Gibreel <m.adilgibreel@gmail.com>
2024-02-01 23:17:53 +0400

    Initial Commit

---
+++ init.lua
@@ -0,0 +1,12 @@
+require "core.launch"
+require "core.options"
+require "core.keymaps"
+-- require "core.autocmds"
+
+-- Packages
+spec "core.colorscheme"
+spec "core.devicons"
+spec "core.treesitter"
+spec "core.mason"
+
+require "core.lazy"
