e74cb896ce1353bdd72cf03d08ad3e7f97e43fa4
Mohamed Gibreel <m.adilgibreel@gmail.com>
2024-02-06 21:03:32 +0400

    feat: adding hint

--- init.lua
+++ init.lua
@@ -1,12 +1,14 @@
 require "core.launch"
 require "core.options"
 require "core.keymaps"
--- require "core.autocmds"
 
--- Packages
-spec "core.colorscheme"
+spec ("core.colorscheme")
 spec "core.devicons"
 spec "core.treesitter"
 spec "core.mason"
+spec "core.schemastore"
+spec "core.lspconfig"
+spec "core.whichkey"
+spec "core.cmp"
 
 require "core.lazy"
