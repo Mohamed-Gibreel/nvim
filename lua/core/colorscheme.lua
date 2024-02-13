5bd700495eb5803876b25b5d052d81634b102df6
Mohamed Gibreel <m.adilgibreel@gmail.com>
2024-02-13 18:20:27 +0400

    chore: revert to darkplus theme

--- lua/core/colorscheme.lua
+++ lua/core/colorscheme.lua
@@ -1,12 +1,13 @@
 local M = {
-  "marko-cerovac/material.nvim",
+  -- "marko-cerovac/material.nvim",
+  "LunarVim/darkplus.nvim",
   lazy = false, -- make sure we load this during startup if it is your main colorscheme
   priority = 1000, -- make sure to load this before all the other start plugins
 }
 
 function M.config()
-  vim.g.material_style = "darker"
-  vim.cmd.colorscheme "material"
+  -- vim.g.material_style = "darker"
+  vim.cmd.colorscheme "darkplus"
 end
 
 return M
