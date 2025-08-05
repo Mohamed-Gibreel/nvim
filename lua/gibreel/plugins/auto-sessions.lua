b92f83f7062d7b1b6af1c35ed47a4de2c3ce15db
Mohamed Gibreel <mgibreel@lenadorsystems.com>
2025-08-05 10:00:10 +0400

    fix(auto-session): enable auto restore

--- lua/gibreel/plugins/auto-sessions.lua
+++ lua/gibreel/plugins/auto-sessions.lua
@@ -3,7 +3,7 @@ return {
 	config = function()
 		local auto_session = require("auto-session")
 		auto_session.setup({
-			auto_restore_enabled = false,
+			auto_restore_enabled = true,
 			auto_session_supress_dirs = { "~/", "~/Downloads", "~/Documents", "~/Desktop/" },
 		})
 
